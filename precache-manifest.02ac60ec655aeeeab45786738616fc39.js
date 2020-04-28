5b6d2a178df78ecfae0f6fc90f88422c03fcdf07
Kattusha <kajra.life@gmail.com>
2020-04-29 00:48:43 +0300

    Updates

---
+++ precache-manifest.02ac60ec655aeeeab45786738616fc39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a722581df1347bdd9502dd204377ee3a",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "bc1a8be34bf9973234c3",
+    "url": "/cards/static/css/main.6f50fb5c.chunk.css"
+  },
+  {
+    "revision": "127f3194f7db52bd8a7c",
+    "url": "/cards/static/js/2.80d16901.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards/static/js/2.80d16901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1a8be34bf9973234c3",
+    "url": "/cards/static/js/main.9251a443.chunk.js"
+  },
+  {
+    "revision": "d9e5b9e134299e4e8d62",
+    "url": "/cards/static/js/runtime-main.670a0fdd.js"
+  },
+  {
+    "revision": "aca69b5cfc4f8e6c741a26faaed1627f",
+    "url": "/cards/static/media/DINNextLTPro-Bold.aca69b5c.woff2"
+  },
+  {
+    "revision": "260449068aae7c5f991a1d43bd9137a7",
+    "url": "/cards/static/media/DINNextLTPro-Light.26044906.woff2"
+  },
+  {
+    "revision": "fd4853d390f73f04c56310b326f7db3f",
+    "url": "/cards/static/media/DINNextLTPro-Medium.fd4853d3.woff2"
+  },
+  {
+    "revision": "c7d8971eea12f6c9a4db5533de266fd2",
+    "url": "/cards/static/media/DINNextLTPro-Regular.c7d8971e.woff2"
+  },
+  {
+    "revision": "3a344931d67b8d02dbc99c27ffc85bfa",
+    "url": "/cards/static/media/logo.3a344931.png"
+  }
+]);
